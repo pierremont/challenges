e26cc16e9c57bb47291a990f66f01d8981990a6f
pierremont <pierre_montaigne@yahoo.com>
2021-01-05 13:40:22 +0200

    new challeges

---
+++ no_oddities_here.py
@@ -0,0 +1,16 @@
+
+'''Write a small function that returns the values of an array that are not odd.
+All values in the array will be integers. Return the good values in the order they are given.
+codewars.com
+'''
+
+def no_odds(values):
+    new_values = []
+    for i in values:
+        if (i % 2) == 0:
+            new_values.append(i)
+    return new_values
+
+
+my_array = [1, 3, 6, 2, 8, 9]
+no_odds(my_array)
